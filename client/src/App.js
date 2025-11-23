20cf8de515114690941a15955dc165e435f892fd
Mack Cutler <maccutler@protonmail.com>
2025-11-22 16:37:41 -0800

    Initial commit: setup client and server structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GroceryGenie</h1>
+      <p>Pantry, recipes, and "What can I make?" coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
